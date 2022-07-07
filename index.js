8e0103056b64fd653148c6f9b2904a6ad513cccd
Shubh-Namdev <s.namdev013@gmail.com>
2022-07-07 18:34:18 +0530

    Worked on game over point

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function startGame() {
     const inputElem = document.getElementById('input');
     const roundsLeftElem = document.getElementById('rounds-left');
+    const winnerElem = document.getElementById("winner");
     let inputValue = inputElem.value;
     roundsLeftElem.textContent = inputValue;
     inputElem.value = '';
@@ -8,92 +9,103 @@ function startGame() {
     if (inputValue > 0) {
         let computerPoints = 0, userPoints = 0;
         checkResult = (msg) => {
-            const choice = ['ROCK', 'PAPER', 'SCISSOR'];
-            const computerChoice = Math.floor(Math.random() * 3);
-            const computerChoiceElem = document.getElementById('computer-select');
-            computerChoiceElem.textContent = choice[computerChoice];
+            if (inputValue > 0) {
+                const choice = ['ROCK', 'PAPER', 'SCISSOR'];
+                const computerChoice = Math.floor(Math.random() * 3);
+                const computerChoiceElem = document.getElementById('computer-select');
+                computerChoiceElem.textContent = choice[computerChoice];
 
-            const midResultElem = document.getElementById('mid-result');
-            const computerPointElem = document.getElementById('computer-point');
-            const userPointsElem = document.getElementById('user-point');
+                const midResultElem = document.getElementById('mid-result');
+                const computerPointElem = document.getElementById('computer-point');
+                const userPointsElem = document.getElementById('user-point');
 
-            switch (msg) {
-                case 'Rock':
-                    if (choice[computerChoice] === 'PAPER') {
-                        midResultElem.textContent = 'LOST'
-                        computerPoints++;
-                        computerPointElem.textContent = computerPoints;
-                        inputValue--;
-                        roundsLeftElem.textContent = inputValue;
+                switch (msg) {
+                    case 'Rock':
+                        if (choice[computerChoice] === 'PAPER') {
+                            midResultElem.textContent = 'LOST'
+                            computerPoints++;
+                            computerPointElem.textContent = computerPoints;
+                            inputValue--;
+                            roundsLeftElem.textContent = inputValue;
 
-                    } else if (choice[computerChoice] === 'ROCK') {
-                        midResultElem.textContent = 'TIE';
-                        computerPoints++;
-                        computerPointElem.textContent = computerPoints;
-                        userPoints++;
-                        userPointsElem.textContent = userPoints;
-                        inputValue--;
-                        roundsLeftElem.textContent = inputValue;
+                        } else if (choice[computerChoice] === 'ROCK') {
+                            midResultElem.textContent = 'TIE';
+                            computerPoints++;
+                            computerPointElem.textContent = computerPoints;
+                            userPoints++;
+                            userPointsElem.textContent = userPoints;
+                            inputValue--;
+                            roundsLeftElem.textContent = inputValue;
 
-                    } else {
-                        midResultElem.textContent = 'WON';
-                        userPoints++;
-                        userPointsElem.textContent = userPoints;
-                        inputValue--
-                        roundsLeftElem.textContent = inputValue;
-                    }
-                    break;
+                        } else {
+                            midResultElem.textContent = 'WON';
+                            userPoints++;
+                            userPointsElem.textContent = userPoints;
+                            inputValue--
+                            roundsLeftElem.textContent = inputValue;
+                        }
+                        break;
 
-                case 'Paper':
-                    if (choice[computerChoice] === 'SCISSOR') {
-                        midResultElem.textContent = 'LOST'
-                        computerPoints++;
-                        computerPointElem.textContent = computerPoints;
-                        inputValue--;
-                        roundsLeftElem.textContent = inputValue;
-                    } else if (choice[computerChoice] === 'PAPER') {
-                        midResultElem.textContent = 'TIE';
-                        computerPoints++;
-                        computerPointElem.textContent = computerPoints;
-                        userPoints++;
-                        userPointsElem.textContent = userPoints;
-                        inputValue--;
-                        roundsLeftElem.textContent = inputValue;
-                    } else {
-                        midResultElem.textContent = 'WON';
-                        userPoints++;
-                        userPointsElem.textContent = userPoints;
-                        inputValue--;
-                        roundsLeftElem.textContent = inputValue;
-                    }
-                    break;
+                    case 'Paper':
+                        if (choice[computerChoice] === 'SCISSOR') {
+                            midResultElem.textContent = 'LOST'
+                            computerPoints++;
+                            computerPointElem.textContent = computerPoints;
+                            inputValue--;
+                            roundsLeftElem.textContent = inputValue;
+                        } else if (choice[computerChoice] === 'PAPER') {
+                            midResultElem.textContent = 'TIE';
+                            computerPoints++;
+                            computerPointElem.textContent = computerPoints;
+                            userPoints++;
+                            userPointsElem.textContent = userPoints;
+                            inputValue--;
+                            roundsLeftElem.textContent = inputValue;
+                        } else {
+                            midResultElem.textContent = 'WON';
+                            userPoints++;
+                            userPointsElem.textContent = userPoints;
+                            inputValue--;
+                            roundsLeftElem.textContent = inputValue;
+                        }
+                        break;
 
-                case 'Scissor':
-                    if (choice[computerChoice] === 'ROCK') {
-                        midResultElem.textContent = 'LOST'
-                        computerPoints++;
-                        computerPointElem.textContent = computerPoints;
-                        inputValue--;
-                        roundsLeftElem.textContent = inputValue;
-                    } else if (choice[computerChoice] === 'SCISSOR') {
-                        midResultElem.textContent = 'TIE';
-                        computerPoints++;
-                        computerPointElem.textContent = computerPoints;
-                        userPoints++;
-                        userPointsElem.textContent = userPoints;
-                        inputValue--;
-                        roundsLeftElem.textContent = inputValue;
-                    } else {
-                        midResultElem.textContent = 'WON';
-                        userPoints++;
-                        userPointsElem.textContent = userPoints;
-                        inputValue--;
-                        roundsLeftElem.textContent = inputValue;
-                    }
-                    break;
+                    case 'Scissor':
+                        if (choice[computerChoice] === 'ROCK') {
+                            midResultElem.textContent = 'LOST'
+                            computerPoints++;
+                            computerPointElem.textContent = computerPoints;
+                            inputValue--;
+                            roundsLeftElem.textContent = inputValue;
+                        } else if (choice[computerChoice] === 'SCISSOR') {
+                            midResultElem.textContent = 'TIE';
+                            computerPoints++;
+                            computerPointElem.textContent = computerPoints;
+                            userPoints++;
+                            userPointsElem.textContent = userPoints;
+                            inputValue--;
+                            roundsLeftElem.textContent = inputValue;
+                        } else {
+                            midResultElem.textContent = 'WON';
+                            userPoints++;
+                            userPointsElem.textContent = userPoints;
+                            inputValue--;
+                            roundsLeftElem.textContent = inputValue;
+                        }
+                        break;
 
+                }
+            }else{
+                if(computerPoints > userPoints){
+                    winnerElem.textContent = "Computer Won";
+                }else if(userPoints > computerPoints){
+                    winnerElem.textContent = "User Won"
+                }else{
+                    winnerElem.textContent = "TIE"
+                } 
             }
         }
+
 
         function gamePlay() {
             const rockElem = document.getElementById('rock');
@@ -114,7 +126,7 @@ function startGame() {
 
         gamePlay();
 
-    }else{
+    } else {
         alert('Chance has been over,Please restart the game.')
     }
 
